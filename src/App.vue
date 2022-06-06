c24df209af97bb435e4d1c24f5820505adda422b
Mohamed Taieb Boussoffara <mohamed.taieb.boussoffara@gmail.com>
2022-06-06 12:25:07 +0200

    Add Answer component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,35 @@
 <template>
-  <p>Arithmetic Quiz</p>
+  <div class="container">
+    <h1>Arithmetic Quiz</h1>
+    <div class="quiz">
+      <component :is="mode" @nextQuestion="handleNextQuestion"></component>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script>
+import Answer from "./components/Answer.vue";
+import Question from "./components/Question.vue";
+
+export default {
+  components: {
+    AppAnswer: Answer,
+    AppQuestion: Question,
+  },
+
+  data() {
+    return {
+      mode: "app-question",
+    };
+  },
+
+  methods: {
+    handleNextQuestion() {
+      this.mode = "app-question";
+    },
+  },
+};
+</script>
 
 <style>
 @import "./assets/css/index.css";
---
+++ src/components/Answer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="question">
+    <h1>That's Correct!</h1>
+    <hr />
+    <button @click="$emit('nextQuestion')">Next Question</button>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.question {
+  padding: 20px;
+  max-width: 500px;
+  background-color: rgba(40, 167, 69, 0.3);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  border-radius: 10px;
+}
+
+.question h1 {
+  color: var(--success);
+}
+
+.question hr {
+  width: 100%;
+  margin: 20px 0 10px 0;
+  border-top: 1px solid #bbbbbb;
+}
+
+.question button {
+  font-size: 18px;
+}
+</style>
